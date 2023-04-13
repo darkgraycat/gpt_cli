dbf133505fb03e561e88d3c39f7fd263e27ac553
darkgraycat <jburyanov@gmail.com>
2023-04-13 16:58:30 +0300

    main: test repl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::io::{self, BufRead};
+
 fn main() {
-    println!("Hello, Rust app!!!");
+    let stdin = io::stdin();
+
+    for line in stdin.lock().lines() {
+        let input = line.expect("Failed to read line");
+        
+        match input.as_str() {
+           "run 1" => println!("Run cmd 1: {}", input),
+           "run 2" => println!("Run cmd 2: {}", input),
+           "run 3" => println!("Run cmd 3: {}", input),
+           "quit" => break,
+           _ => println!("Unknown command {}", input),
+        }
+    }
 }
