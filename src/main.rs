0cbf82f7fc2e223dd2aa5adf0200eed87cd0347b
darkgraycat <jburyanov@gmail.com>
2023-04-13 22:26:22 +0300

    main: use own write read

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,31 @@
-use std::io::{self, BufRead};
+use std::io::{self, BufRead, Write};
+
+const STRINGS_WELCOME: &str = "ChatGPT CLI v0.00\nType 'help' to see commands";
+const STRINGS_HELP: &str = "ChatGPT CLI help page\nquit - close application";
+const STRINGS_UNKNOWN: &str = "Unknown command";
+
+fn write_line(msg: &str) {
+    io::stdout()
+        .lock()
+        .write_all((msg.to_owned() + "\n").as_bytes())
+        .unwrap();
+}
+
+fn read_line() -> String {
+    let mut input = String::with_capacity(256);
+    io::stdin().lock().read_line(&mut input).unwrap();
+    input.truncate(input.trim().len());
+    input
+}
 
 fn main() {
-    let stdin = io::stdin();
-
-    for line in stdin.lock().lines() {
-        let input = line.expect("Failed to read line");
-        
+    write_line(STRINGS_WELCOME);
+    loop {
+        let input = read_line();
         match input.as_str() {
-           "run 1" => println!("Run cmd 1: {}", input),
-           "run 2" => println!("Run cmd 2: {}", input),
-           "run 3" => println!("Run cmd 3: {}", input),
-           "quit" => break,
-           _ => println!("Unknown command {}", input),
+            "help" => write_line(STRINGS_HELP),
+            "quit" => break,
+            _ => write_line(STRINGS_UNKNOWN),
         }
     }
 }
