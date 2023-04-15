922643783833794337da50323f95764448120b85
darkgraycat <jburyanov@gmail.com>
2023-04-16 00:15:30 +0300

    main: use reqwest+serde to fetch and parse json

--- src/http.rs
+++ src/http.rs
@@ -1,19 +1,2 @@
-use std::io::{BufRead, BufReader, Read, Result, Write};
-use std::net::TcpStream;
 
-pub enum HttpMethod {
-    Get,
-    Post,
-}
 
-pub fn make_request(host: &str, port: u16, method: HttpMethod, endpoint: &str) -> Result<String> {
-    let request = match method {
-        HttpMethod::Get => format!("GET /{} HTTP/1.1\r\nHost: {}\r\n\r\n", endpoint, host),
-        HttpMethod::Post => format!("POST /{} HTTP/1.1\r\nHost: {}\r\n\r\n", endpoint, host),
-    };
-    let mut response = String::new();
-    let mut stream = TcpStream::connect(format!("{}:{}", host, port))?;
-    stream.write_all(request.as_bytes())?;
-    BufReader::new(stream).read_to_string(&mut response)?;
-    Ok(response)
-}
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,34 @@
 mod macros;
-mod http;
 
-#[derive(Debug)]
-#[derive(Default)]
+use reqwest::blocking::get;
+use serde::Deserialize;
+
+#[derive(Default, Debug)]
 struct Session {
     apikey: String,
+}
+
+fn print_jokes() {
+    #[derive(Deserialize)]
+    struct ChuckNorrisJoke {
+        value: String,
+        icon_url: String,
+    }
+    let url = "https://api.chucknorris.io/jokes/random";
+    loop {
+        let response: ChuckNorrisJoke = get(url).unwrap().json().unwrap();
+        println!("{}", response.value);
+        print!("Want more? ");
+        if readln!() == "no" {
+            break;
+        }
+    }
 }
 
 fn main() {
     //let mut session = Session::default();
     //print!("Enter apikey > ");
     //session.apikey = readln!();
-
-    use http::HttpMethod::Get;
-    let resp = http::make_request("api.chucknorris.io", 443, Get, "/jokes/random");
-    match resp {
-        Ok(data) => println!("Data: {}", data),
-        Err(err) => eprintln!("Error {}", err),
-    }
+    println!("Hi");
+    print_jokes();
 }
