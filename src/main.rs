0cfccd0d6c5056c33d661c2027325ee7eaf9fbae
darkgraycat <jburyanov@gmail.com>
2023-04-16 03:31:49 +0300

    main: implement QA session in the loop

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,17 @@ use response::Response;
 
 const CHAT_GPT_URL: &'static str = "https://api.openai.com/v1/chat/completions";
 const CHAT_GPT_MODEL: &'static str = "gpt-3.5-turbo";
+const CHAT_GPT_SYSINFO: &'static str = "Please provide short answers";
 
 fn ask_gpt(session: &mut Session, question: &str) -> Result<Response, Error> {
     let payload = format!(
         r#"{{
-        "model": "{}",
-        "messages": [
-            {{ "role": "system", "content": "{}" }},
-            {{ "role": "user", "content": "{}" }}
-        ]
-    }}"#,
+            "model": "{}",
+            "messages": [
+                {{ "role": "system", "content": "{}" }},
+                {{ "role": "user", "content": "{}" }}
+            ]
+        }}"#,
         CHAT_GPT_MODEL, session.system_info, question
     );
     let bearer_apikey = format!("Bearer {}", session.apikey);
@@ -40,9 +41,17 @@ fn main() {
     let mut session = Session::default();
     print!("Enter apikey > ");
     session.apikey = readln!();
-    session.system_info = "Please provide short answers".to_owned();
+    session.system_info = CHAT_GPT_SYSINFO.to_owned();
 
-    let response = ask_gpt(&mut session, "How do you feel today");
-    println!("R:{:?}", &response);
-    println!("Answer {}", response.unwrap().parse());
+    println!("Type \'quit\' to close application");
+    loop {
+        print!("--------------------------------\n> ");
+        let input = readln!();
+        if input == "quit" { break; }
+        let response = ask_gpt(&mut session, &input);
+        match response {
+            Ok(r) => println!("{}", r.parse()),
+            Err(err) => eprintln!("Error occured: {}", err),
+        }
+    }
 }
