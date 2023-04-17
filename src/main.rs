1903c972ab9b3fbf8140b2a126561735d75f4220
darkgraycat <jburyanov@gmail.com>
2023-04-17 17:10:48 +0300

    main: refactor 2

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,12 @@ use session::Session;
 const CHAT_GPT_URL: &'static str = "https://api.openai.com/v1/chat/completions";
 const CHAT_GPT_MODEL: &'static str = "gpt-3.5-turbo";
 const DEFAULT_SYS_USER_MESSAGE: &'static str = "Please provide short answers";
-const SEPARATOR: &'static str = "\x1b[31m---------------- type 'quit' to exit ----------------\x1b[0m";
+const SEPARATOR: &'static str = "---------------- type 'quit' to exit ----------------";
+
+const CLR_RESET: &'static str = "\x1b[0m";
+const CLR_INPUT: &'static str = "\x1b[32m";
+const CLR_SEPARATOR: &'static str = "\x1b[31m";
+const CLR_CODEBLOCK: &'static str = "\x1b[36m";
 
 fn ask_gpt(session: &mut Session<Message>) -> Result<Response, Error> {
     let payload = serde_json::to_string(&session).unwrap();
@@ -28,19 +33,22 @@ fn format_gtp_answer(msg: &str) -> String {
     let mut output = String::new();
     let mut is_code = false;
     for chunk in msg.split("```") {
-        let color = if is_code { "\x1b[36m" } else {"\x1b[0m" };
-        output.push_str(&format!("{}{}", color, chunk));
+        output.push_str(&format!(
+            "{}{}",
+            if is_code { CLR_CODEBLOCK } else { CLR_RESET },
+            chunk
+        ));
         is_code = !is_code;
     }
     output
 }
 
 fn main() {
-    print!("Enter apikey\n\x1b[32m> \x1b[0m");
+    print!("Enter apikey\n{}> {}", CLR_INPUT, CLR_RESET);
     let input = readln!();
     let mut session = Session::<Message>::new(input, String::from(CHAT_GPT_MODEL));
 
-    print!("Enter context or leave empty\n\x1b[32m> \x1b[0m");
+    print!("Enter context or leave empty\n{}> {}", CLR_INPUT, CLR_RESET);
     let input = readln!();
 
     session.push_message(Message::new(
@@ -53,7 +61,10 @@ fn main() {
     ));
 
     loop {
-        print!("{}\n\x1b[32m> \x1b[0m", SEPARATOR);
+        print!(
+            "{}{}{}\n{}> {}",
+            CLR_SEPARATOR, SEPARATOR, CLR_RESET, CLR_INPUT, CLR_RESET
+        );
         let input = readln!();
         if input == "quit" {
             break;
@@ -64,7 +75,6 @@ fn main() {
             Ok(r) => {
                 let message = r.get_message();
                 println!("{}", format_gtp_answer(&message.content));
-                //println!("{}", message.content);
                 session.push_message(message);
             }
             Err(err) => eprintln!("Error occured: {}", err),
